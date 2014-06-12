a0cd36770a97507bbbcdfbe5d920b9da712219ce
Peggy Griffin <seishingithub@gmail.com>
2014-06-12 14:02:22 -0600

    User can edit and update fruits in list

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -6,7 +6,7 @@ class FruitsController < ApplicationController
   end
 
   def create
-    @fruit = Fruit.create(fruits_params)
+    @fruit = Fruit.create(fruit_params)
     if @fruit.save
       redirect_to fruits_path
     else
@@ -14,9 +14,24 @@ class FruitsController < ApplicationController
     end
   end
 
+  def show
+    @fruit = Fruit.find(params[:id])
+  end
+
+  def edit
+    @fruit = Fruit.find(params[:id])
+  end
+
+  def update
+    @fruit = Fruit.find(params[:id])
+    @fruit.update_attributes!(fruit_params)
+
+    redirect_to fruits_path
+  end
+
   private
 
-  def fruits_params
+  def fruit_params
     params.require(:fruit).permit(:name, :color)
   end
 end
--- spec/features/fruits_spec.rb
+++ spec/features/fruits_spec.rb
@@ -10,6 +10,22 @@ feature 'Manage Fruits' do
     click_on 'Create fruit'
     expect(page).to have_content 'banana'
     expect(page).to have_content 'yellow'
+  end
 
+  scenario 'User can edit and update list of fruits' do
+    visit '/'
+    click_on 'Add fruit'
+    fill_in 'Name', with: 'banana'
+    fill_in 'Color', with: 'yellow'
+    click_on 'Create fruit'
+    expect(page).to have_content 'banana'
+    expect(page).to have_content 'yellow'
+    click_on 'banana'
+    expect(page).to have_content 'banana'
+    expect(page).to have_content 'yellow'
+    click_on 'Edit fruit'
+    fill_in 'Name', with: 'banana'
+    fill_in 'Color', with: 'yellow'
+    click_on 'Update fruit'
   end
 end
